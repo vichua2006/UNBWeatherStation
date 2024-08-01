8dc89fb0e3d9c4437fd78703cc07772e1d9c52f7
Victor Huang <vichua2006@gmail.com>
2024-08-01 11:12:20 -0300

    added env calibration values and updated gitignore

--- bme280_sample.py
+++ bme280_sample.py
@@ -1,8 +1,10 @@
+import os
 import time
 import pytz
 import datetime
 import smbus2
 import bme280
+from dotenv import load_dotenv
 
 # BME280 sensor address (default address)
 address = 0x77
@@ -15,6 +17,15 @@ calibration_params = bme280.load_calibration_params(bus, address)
 
 # Load timezone 
 halifax_tz = pytz.timezone("America/Halifax")
+
+# Load env variables
+load_dotenv()
+
+# Load device name and calibration 
+device_name = os.getenv("NAME")
+TEMP_CALI = float(os.getenv("TEMP_CALI"))
+PRESSURE_CALI = float(os.getenv("PRESSURE_CALI"))
+HUMIDITY_CALI = float(os.getenv("HUMIDITY_CALI"))
 
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
@@ -30,7 +41,7 @@ def main():
 
     # Init csv file header
     date = datetime.datetime.now().strftime("%Y-%m-%d") 
-    with open(rf"data/{date}.csv", "w") as file:
+    with open(rf"data/{date}_{device_name}.csv", "w") as file:
         header_str = ",".join(header)
         file.write(header_str + "\n")
 
@@ -42,9 +53,9 @@ def main():
 
                 # Extract temperature, pressure, and humidity
                 timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                temperature_celsius = data.temperature
-                pressure = data.pressure
-                humidity = data.humidity
+                temperature_celsius = data.temperature + TEMP_CALI
+                pressure = data.pressure + PRESSURE_CALI
+                humidity = data.humidity + HUMIDITY_CALI
 
                 # Convert temperature to Fahrenheit
                 # temperature_fahrenheit = celsius_to_fahrenheit(temperature_celsius)
@@ -59,6 +70,8 @@ def main():
 
                 file.write(reading_str)
                 file.write("\n")
+
+                print(reading_str)
 
                 # Wait for a few seconds before the next reading
                 time.sleep(0.5)
--- ds3231.py
+++ ds3231.py
@@ -1,1 +1,12 @@
-# sample code for the ds3231 Real Time Clock (RTC)
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Load device name and calibration 
+device_name = os.getenv("NAME")
+TEMP_CALI = os.getenv("TEMP_CALI")
+PRESSURE_CALI = os.getenv("PRESSURE_CALI")
+HUMIDITY_CALI = os.getenv("HUMIDITY_CALI")
+
+print(device_name, TEMP_CALI, PRESSURE_CALI, HUMIDITY_CALI)
