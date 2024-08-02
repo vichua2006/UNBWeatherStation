c58cb3c149d7e4a9c14fddacf3aa75164ac8db95
Victor <vichua2006@gmail.com>
2024-08-02 14:11:23 -0300

    aug 2 julie measurement

--- start_script.sh
+++ start_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # To record on startup
-. /home/vichua2006/Desktop/UNBWeatherStation/venv/bin/activate
-python /home/vichua2006/Desktop/UNBWeatherStation/bme280_sample.py
+. /home/julmar84/Desktop/UNBWeatherStation/venv/bin/activate
+python /home/julmar84/Desktop/UNBWeatherStation/bme280_sample.py
 
 # for the RTC; must be before the script to have accurate time
 echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
