8784d10dca81075ef17b874008d25a4b99d17dc8
Victor Huang <vichua2006@gmail.com>
2024-08-02 14:25:16 -0300

    saved new shell script

--- start_script.sh
+++ start_script.sh
@@ -1,15 +1,5 @@
 # #!/bin/bash
 
 # To record on startup
-<<<<<<< HEAD
 . /home/UNBWeatherStation/venv/bin/activate
 python /home/UNBWeatherStation/bme280_sample.py
-=======
-. /home/julmar84/Desktop/UNBWeatherStation/venv/bin/activate
-python /home/julmar84/Desktop/UNBWeatherStation/bme280_sample.py
-
-
-# for the RTC; must be before the script to have accurate time
-echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
-sudo hwclock -s
->>>>>>> 5b16441c9226b46aaea0537502c88250ef12157e
